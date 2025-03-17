b9b6541a4863c0d16e4eb48a0143564105cc55be
Ulephraim <iphraimunlimited@gmail.com>
2025-03-17 22:51:49 +0100

    project folder structure setup

---
+++ src/components/dashboard/Dashboard.tsx
@@ -0,0 +1,9 @@
+/** @format */
+
+import React from 'react';
+
+const Dashboard = () => {
+  return <div className="item-center text-red-500">Dashboard</div>;
+};
+
+export default Dashboard;
---
+++ src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,14 @@
+/** @format */
+
+import React from 'react';
+import Dashboard from '../dashboard/Dashboard';
+
+const Sidebar = () => {
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  );
+};
+
+export default Sidebar;
